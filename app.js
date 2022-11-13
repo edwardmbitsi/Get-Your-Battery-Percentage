1e1c31883865acd2ff30abe89ec5f54eb051a5c0
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-11-13 18:09:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,6 @@ navigator.getBattery().then(function(battery) {
 
 navigator.getBattery().then(function(battery) {
     $('.progress-bar').css('width', battery.level * 100 + '%');
+    $('#level').html('Battery level: ' + (battery.level * 100).toFixed() + '%')
+
 
