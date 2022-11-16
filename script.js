e0ff807eb63a6645a121983f4c518eab51e1f8f0
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-11-16 19:26:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-navigator.getBattery().then(function(battery) {
-    $('.progress-bar').css('width', battery.level * 100 + '%');
-    $('#level').html('Battery level: ' + (battery.level * 100).toFixed() + '%')
-    battery.onlevelchange = function() {
-        $('#level').html('Battery level: ' + (this.level * 100).toFixed() + '%')
-        $('.progress-bar').css('width', this.level * 100 + '%');
-    };
-});
-
-navigator.getBattery().then(function(battery) {
-    $('.progress-bar').css('width', battery.level * 100 + '%');
-    $('#level').html('Battery level: ' + (battery.level * 100).toFixed() + '%')
-
-battery.onlevelchange = function() {
-     $('#level').html('Battery level: ' + (this.level * 100).toFixed() + '%')
-     $('.progress-bar').css('width', this.level * 100 + '%');
-   };
-});
+const chargeLevel = document.getElementById("charge-level");
+const charge = document.getElementById("charge");
+const chargingTimeRef = document.getElementById("charging-time");
+window.onload = () => {
